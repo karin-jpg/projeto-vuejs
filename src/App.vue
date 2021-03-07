f262c61f454366a5cde1e93ed8c60f1f0bcfdff5
karin-jpg <andradkarin@gmail.com>
2021-03-07 14:27:27 -0300

    Adição de pasta para separação de componentes reutilizaveis

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,10 @@
 <h1 class="centralizado">{{titulo}}</h1>
 <ul class="lista-fotos">
   <li class="lista-fotos-item" v-for= "foto of fotos" :key= "foto.id">
-    <div class="painel">
-      <h2 class="painel-titulo"> {{foto.titulo}}</h2>
-      <div class="painel-conteudo">
-        <img class="imagem-responsiva" :src = " foto.url " :alt = "foto.titulo">
-      </div>
-    </div>
+
+    <meu-painel :titulo= "foto.titulo">
+      <img class="imagem-responsiva" :src= "foto.url" :alt = "foto.titulo">
+    </meu-painel>
     
     </li>
 </ul>
@@ -19,7 +17,13 @@
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue'
+
 export default {
+
+  components: {
+    'meu-painel': Painel
+  },
 
   data(){
 
@@ -55,27 +59,6 @@ export default {
 
   .lista-fotos .lista-fotos-item {
     display: inline-block;
-  }
-
-  .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
   }
 
   .imagem-responsiva {
