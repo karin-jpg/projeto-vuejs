73a3e4256231f1d57b233085a9a14d1d0fc27aa3
karin-jpg <andradkarin@gmail.com>
2021-03-08 19:37:03 -0300

    Adicionada transição nos paineis

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="painel">
       <h2 class="painel-titulo" @dblclick = "visivel = !visivel"> {{ titulo }} </h2>
+      <transition name = "painel-fade">
       <div class="painel-conteudo" v-show= "visivel">
         <slot>
        </slot>
       </div>
+      </transition>
     </div>
 
 </template>
@@ -45,6 +47,14 @@ export default {
     margin: 0 0 15px 0;
     padding: 10px;
     text-transform: uppercase;
+  }
+
+  .painel-fade-enter, .painel-fade-leave-active {
+    opacity: 0
+  }
+
+  .painel-fade-enter-active, .painel-fade-leave-active{
+    transition: opacity .3s;
   }
 
 </style>
